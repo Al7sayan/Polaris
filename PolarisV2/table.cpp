774e1783f5190571df79864eb2737b3ec9699ac8
irma <68775263+omairma@users.noreply.github.com>
2020-10-09 20:17:05 +0200

    Better state machine

--- PolarisV2/table.cpp
+++ PolarisV2/table.cpp
@@ -4,14 +4,14 @@ namespace polaris
 {
     void Table::Update()
     {
-        for (auto i = m_lPlates.begin(); i != m_lPlates.end(); ++i)
-            (*i)->Update();
+        if (m_lPlates.size() > 0)
+            m_lPlates.front()->Update();
     }
 
     void Table::PushPlate(Plate* plate)
     {
         // NOTE: If there's no plates yet, not having this check will cause a crash.
-        if(m_lPlates.front() != nullptr)
+        if (m_lPlates.size() > 0)
             m_lPlates.front()->OnDisabled();
 
         m_lPlates.push_front(plate);
@@ -19,12 +19,14 @@ namespace polaris
     }
     void Table::PopPlate()
     {
-        // NOTE: If there's no plates yet, not having this check will cause a crash.
-        if (m_lPlates.front() != nullptr)
+        if (m_lPlates.size() > 0)
+        {
             m_lPlates.front()->OnDisabled();
+            m_lPlates.pop_front();
 
-        m_lPlates.pop_front();
-        m_lPlates.front()->OnEnabled();
+            if (m_lPlates.size() > 0)
+                m_lPlates.front()->OnEnabled();
+        }
     }
     void Table::Pop()
     {
