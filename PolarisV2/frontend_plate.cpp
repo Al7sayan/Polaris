a7d93e07aafb92a7444023b7fd40237a6fc420b1
irma <68775263+omairma@users.noreply.github.com>
2020-10-27 17:46:22 +0100

    FUCKING HELL

--- PolarisV2/frontend_plate.cpp
+++ PolarisV2/frontend_plate.cpp
@@ -2,6 +2,7 @@
 #include "globals.h"
 #include "program.h"
 #include "athena_plate.h"
+#include "error_utils.h"
 
 #include <iostream>
 
@@ -21,8 +22,13 @@ namespace polaris
 
             void FrontendPlate::ProcessEventHook(SDK::UObject* pObject, SDK::UFunction* pFunction, PVOID pParams)
             {
-                if (pFunction->GetName().find("BP_PlayButton") != std::string::npos)
-                    gpProgram->m_pMainTable->PushPlate(new AthenaPlate);
+                if (globals::gpPlayerController != nullptr)
+                {
+                    if (pFunction->GetName().find("BP_PlayButton") != std::string::npos)
+                        gpProgram->m_pMainTable->PushPlate(new AthenaPlate);
+                }
+                else
+                    utilities::ErrorUtils::ThrowException(L"YOUR COMPUTER FUCKING SUCKS ASS!!!!!!");
             }
         }
     }
